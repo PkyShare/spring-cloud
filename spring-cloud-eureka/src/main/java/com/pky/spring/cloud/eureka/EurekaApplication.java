6d40535882e5b52af5be6e591f69a03cddb5ce7e
signv <aa381462886aa@163.com>
2019-07-31 18:38:53 +0800

    1.初始化springcloud netflix 2.完成服务注册与发现中心

---
+++ spring-cloud-eureka/src/main/java/com/pky/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.pky.spring.cloud.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  //开启服务注册与发现
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
