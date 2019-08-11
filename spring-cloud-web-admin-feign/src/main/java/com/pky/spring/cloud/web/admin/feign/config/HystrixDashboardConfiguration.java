358759b15a0aaec2a7b5166f4039dafd2987e563
1689936851@qq.com <abc3780926745>
2019-08-11 22:58:38 +0800

    1.dashboard完成

---
+++ spring-cloud-web-admin-feign/src/main/java/com/pky/spring/cloud/web/admin/feign/config/HystrixDashboardConfiguration.java
@@ -0,0 +1,26 @@
+package com.pky.spring.cloud.web.admin.feign.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Boot 2.x 版本开启 Hystrix Dashboard 与 Spring Boot 1.x 的方式略有不同，
+ * 需要增加一个 HystrixMetricsStreamServlet 的配置
+ */
+@Configuration
+public class HystrixDashboardConfiguration {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        // 因为没有web.xml，springboot-web-servlet 提供的创建 servlet 的方式。
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        // 注册 servlet
+        registrationBean.setLoadOnStartup(1);   // servlet 启动顺序
+        registrationBean.addUrlMappings("/hystrix.stream");  // servlet 的 mapping
+        registrationBean.setName("HystrixMetricsStreamServlet");  // servlet 的 name
+        return registrationBean;
+    }
+}
