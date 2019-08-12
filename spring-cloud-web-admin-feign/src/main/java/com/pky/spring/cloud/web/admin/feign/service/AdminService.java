d07656e40b8159daa65fe8ee7e5182750d5f7dfe
signv <aa381462886aa@163.com>
2019-08-12 18:43:18 +0800

    1. Zuul API 网关

--- spring-cloud-web-admin-feign/src/main/java/com/pky/spring/cloud/web/admin/feign/service/AdminService.java
+++ spring-cloud-web-admin-feign/src/main/java/com/pky/spring/cloud/web/admin/feign/service/AdminService.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "admin-provider", path = "/admin", configuration = FeignConfiguration.class, fallback = AdminServiceHystrix.class)
+@FeignClient(value = "admin-provider", path = "admin", configuration = FeignConfiguration.class, fallback = AdminServiceHystrix.class)
 public interface AdminService {
 
     @GetMapping(value = "")
